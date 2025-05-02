928299ac0ed8d61e209125b2c610e487ac3f5732
Regersays <164368522+Regersays69@users.noreply.github.com>
2025-05-02 19:45:16 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sysctl -w net.ipv6.conf.all.disable_ipv6=1 >/dev/null 2>&1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
-REPO="http://myrid.my.id/os/"
+REPO="https://v4.serverpremium.web.id:81/os/"
 REPO2="https://raw.githubusercontent.com/Regersays69/os/main/"
 function CEKIP () {
 MYIP=$(curl -sS ipv4.icanhazip.com)
